a045b23f21fc9329b71066a520f24ddc2a3b2748
Aaron D Borden <adborden@a14n.net>
2018-10-05 15:46:26 -0700

    Solr specific tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,26 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+solr_port = 8983
+solr_core = 'catalog'
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_solr_service(host):
+    solr = host.service('solr')
+
+    assert solr.is_running
+    assert solr.is_enabled
+
+
+def test_solr_port(host):
+    socket = host.socket('tcp://0.0.0.0:8983')
+
+    assert socket.is_listening
+
+
+def test_solr_core(host):
+    host.ansible(
+        'uri',
+        'url=http://localhost:%s/solr/%s/get' % (solr_port, solr_core),
+        check=False
+        )
